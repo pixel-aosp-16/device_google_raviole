ed69c3df824cd7ff40473d81fa58c7eb38b660be
flawedworld <flawedworld@flawed.world>
2025-06-14 03:43:26 +0000

    AOSP targets cleanup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -20,26 +20,12 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_oriole_pgagnostic_64.mk \
     $(LOCAL_DIR)/aosp_oriole_pkvm.mk \
     $(LOCAL_DIR)/aosp_oriole_64.mk \
-    $(LOCAL_DIR)/factory_oriole.mk \
     $(LOCAL_DIR)/aosp_raven.mk \
     $(LOCAL_DIR)/aosp_raven_hwasan.mk \
-    $(LOCAL_DIR)/haiku.mk \
     $(LOCAL_DIR)/aosp_raven_pgagnostic_64.mk \
     $(LOCAL_DIR)/aosp_raven_pkvm.mk \
     $(LOCAL_DIR)/aosp_raven_64.mk \
-    $(LOCAL_DIR)/factory_raven.mk \
-    $(LOCAL_DIR)/aosp_slider.mk \
-    $(LOCAL_DIR)/aosp_slider_hwasan.mk \
-    $(LOCAL_DIR)/factory_slider.mk \
-    $(LOCAL_DIR)/full_slider.mk \
-    $(LOCAL_DIR)/aosp_whitefin.mk \
-    $(LOCAL_DIR)/aosp_whitefin_pkvm.mk \
-    $(LOCAL_DIR)/aosp_whitefin_hwasan.mk \
-    $(LOCAL_DIR)/aosp_whitefin_64.mk \
-    $(LOCAL_DIR)/factory_whitefin.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_slider-trunk_staging-userdebug \
-    aosp_whitefin-trunk_staging-userdebug \
     aosp_oriole-trunk_staging-userdebug \
     aosp_raven-trunk_staging-userdebug
